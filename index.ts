27bb24cb8666c0b56a2ff3f60b6009111bf4c6a3
Andrew <growler625@gmail.com>
2022-06-05 19:02:33 +0300

    eject cors

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import cors from 'cors';
+// import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(cors());
+// app.use(cors());
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
